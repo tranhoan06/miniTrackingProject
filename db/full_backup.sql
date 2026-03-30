afaa4b7388b04c3ba97d9f8056c9a4adf7edd35c
tranviethoan <tranviethoan@luvina.net>
2026-03-30 17:34:05 +0700

    main

---
+++ db/full_backup.sql
@@ -0,0 +1,221 @@
+-- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
+--
+-- Host: localhost    Database: minitrackingproject
+-- ------------------------------------------------------
+-- Server version	8.0.43
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `addreses`
+--
+
+DROP TABLE IF EXISTS `addreses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `addreses` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL,
+  `receiver_name` varchar(255) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  `province_id` int NOT NULL,
+  `province_name` varchar(255) NOT NULL,
+  `district_id` int NOT NULL,
+  `district_name` varchar(255) DEFAULT NULL,
+  `ward_id` int NOT NULL,
+  `ward_name` varchar(255) DEFAULT NULL,
+  `detail_address` varchar(500) DEFAULT NULL,
+  `is_Delete` tinyint(1) DEFAULT '0',
+  `is_default` tinyint(1) DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `addreses_ibfk_1` (`user_id`),
+  CONSTRAINT `addreses_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `addreses`
+--
+
+LOCK TABLES `addreses` WRITE;
+/*!40000 ALTER TABLE `addreses` DISABLE KEYS */;
+INSERT INTO `addreses` VALUES (1,1,'Trần Việt Hoàn','012345678',1,'Ninh Bình',2,'Hoa Lư',3,'Test','Ngõ 67 Vạn Xuân 1',0,1,'2026-03-29 14:19:02','2026-03-30 07:47:28'),(3,2,'Trần Việt Hoàn','012345678',1,'Ninh Bình',2,'Hoa Lư',3,'Test','Ngõ 67 Vạn Xuân 1',0,0,'2026-03-29 14:27:22','2026-03-30 07:40:24'),(4,1,'hoàn2','123456789',1,'Ninh Bình',2,'Hoa Lư',3,'Test','K có',0,0,'2026-03-29 14:28:31',NULL),(5,1,'hoàn2','123456789',1,'Ninh Bình',2,'Hoa Lư',3,'Test','K có',0,0,'2026-03-29 14:38:32',NULL),(6,1,'hoàn2','123456789',1,'Ninh Bình',2,'Hoa Lư',3,'Test','K có',0,0,'2026-03-29 14:40:50',NULL),(7,1,'hoàn10','123456789',1,'Ninh Bình',2,'Hoa Lư',3,'Test','K có',0,0,'2026-03-29 14:43:21',NULL),(8,1,'hoàn7','123456789',1,'Ninh Bình',2,'Hoa Lư',3,'Test','K có',0,0,'2026-03-29 14:46:18',NULL),(9,1,'hoàn7','123456789',1,'Ninh Bình',2,'Hoa Lư',3,'Test','K có',0,0,'2026-03-29 14:46:40',NULL),(10,1,'hoàn7','123456789',1,'Ninh Bình',2,'Hoa Lư',3,'Test','K có',0,0,'2026-03-29 15:07:03',NULL),(11,1,'hoàn7','123456789',1,'Ninh Bình',2,'Hoa Lư',3,'Test','K có',0,0,'2026-03-29 15:07:28',NULL),(12,1,'hoàn7','123456789',1,'Ninh Bình',2,'Hoa Lư',3,'Test','K có',0,0,'2026-03-29 15:07:32',NULL),(13,1,'hoàn7','123456789',1,'Ninh Bình',2,'Hoa Lư',3,'Test','K có',0,0,'2026-03-29 15:10:32',NULL),(14,1,'hoàn7','123456789',1,'Ninh Bình',2,'Hoa Lư',3,'Test','K có',1,0,'2026-03-29 15:13:51','2026-03-30 07:44:31'),(15,2,'Quân','012345678',1,'Ninh Bình',2,'Hoa Lư',3,'Test','Test Mapstruct',1,0,'2026-03-30 06:55:40','2026-03-30 07:38:11');
+/*!40000 ALTER TABLE `addreses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categories` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(200) NOT NULL,
+  `parent_id` bigint DEFAULT NULL,
+  `is_active` tinyint(1) NOT NULL DEFAULT '1',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `parent_id` (`parent_id`),
+  CONSTRAINT `categories_ibfk_1` FOREIGN KEY (`parent_id`) REFERENCES `categories` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `inventory`
+--
+
+DROP TABLE IF EXISTS `inventory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `inventory` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `product_id` bigint NOT NULL,
+  `quantity_in_stock` int NOT NULL DEFAULT '0',
+  `reserved_quantity` int NOT NULL DEFAULT '0',
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `product_id` (`product_id`),
+  CONSTRAINT `inventory_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inventory`
+--
+
+LOCK TABLES `inventory` WRITE;
+/*!40000 ALTER TABLE `inventory` DISABLE KEYS */;
+/*!40000 ALTER TABLE `inventory` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_images`
+--
+
+DROP TABLE IF EXISTS `product_images`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product_images` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `product_id` bigint NOT NULL,
+  `image_url` varchar(500) NOT NULL,
+  `is_thumbnail` tinyint(1) DEFAULT '0',
+  `sort_order` int DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `product_id` (`product_id`),
+  CONSTRAINT `product_images_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_images`
+--
+
+LOCK TABLES `product_images` WRITE;
+/*!40000 ALTER TABLE `product_images` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product_images` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `products` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `seller_id` bigint NOT NULL,
+  `category_id` bigint NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `description` text,
+  `price` decimal(15,2) NOT NULL,
+  `original_price` decimal(15,2) DEFAULT NULL,
+  `weight_gram` decimal(10,2) NOT NULL,
+  `length_cm` decimal(6,2) DEFAULT NULL,
+  `width_cm` decimal(6,2) DEFAULT NULL,
+  `height_cm` decimal(6,2) DEFAULT NULL,
+  `sku` varchar(255) DEFAULT NULL,
+  `status` varchar(30) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `seller_id` (`seller_id`),
+  KEY `category_id` (`category_id`),
+  CONSTRAINT `products_ibfk_1` FOREIGN KEY (`seller_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `products_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `fullname` varchar(255) DEFAULT NULL,
+  `role` varchar(50) DEFAULT NULL,
+  `create_at` timestamp NULL DEFAULT NULL,
+  `update_at` timestamp NULL DEFAULT NULL,
+  `is_Delete` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'hoan1','$2a$10$XTiwpuyBBiOmW6x8n2BS5eL4Eh/TOVmBaOKC2SnXuuG1hS5dzh7NC','hoan11','SELLER','2026-03-29 08:53:09','2026-03-29 09:17:09',0),(2,'hoan2','$2a$10$nJYf1KqS2hrg4vVM3aaU9u3fiDHW2t5hANFl4n4494EdjZXRIJUP2','hoan2','BUYER','2026-03-29 08:57:41',NULL,0),(3,'hoan3','$2a$10$/HC998rbtXcnCEanWVtQ7Ok.aZj8lm1XbOKjGM1Nc55zIbR4SQqhS','hoan3','SHIPPER','2026-03-29 09:16:52',NULL,0),(4,'hoan4','$2a$10$hCssKUftpqKp5ltxmdtzUuSmqAOmC4IqA..y4pm.Mq/fovnN6UN2.','hoan3','SHIPPER','2026-03-29 09:27:29',NULL,0),(5,'hoan5','$2a$10$005Na/EQQ7PW4MQTB6TUUOjY941HXnNzVUg4XLSVuhyZ7irkg8yUe','hoan5','SHIPPER','2026-03-29 12:48:35',NULL,0);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-03-30 17:33:34
--- src/main/java/com/example/miniTrackingProject/entity/ProductsEntity.java
+++ src/main/java/com/example/miniTrackingProject/entity/ProductsEntity.java
@@ -1,5 +1,21 @@
 package com.example.miniTrackingProject.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-public class ProductsEntity {
+import java.io.Serializable;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductsEntity implements Serializable {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
 }
